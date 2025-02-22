71b625d2085c9818735d9d37e0b9162f0f9214c3
Erimav <vladik50104@gmail.com>
2025-02-23 01:39:45 +0200

    UI Revorks

--- Code/UI/Menu.cs
+++ Code/UI/Menu.cs
@@ -1,12 +1,19 @@
 using Godot;
 using System;
 
-public partial class MainMenu : Node
+public partial class Menu : Node
 {
     [Export]
     private PackedScene eMainScene;
+    [Export]
+    private PackedScene mMainMenuScene;
 
     public void NewGame()
+    {
+        GetTree().ChangeSceneToPacked(eMainScene);
+    }
+
+    public void MainMenu()
     {
         GetTree().ChangeSceneToPacked(eMainScene);
     }
